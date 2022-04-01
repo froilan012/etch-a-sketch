958824bc1e13fbce5af6a4082f08668bf45c6697
Froilan <froilan.i.fajardo@gmail.com>
2022-04-01 13:27:30 +0800

    Create sketchpad and add mosehover eventlistner

---
+++ script.js
@@ -0,0 +1,44 @@
+const btn1 = document.querySelector('#one');
+btn1.addEventListener('mouseover', () =>{
+    console.log("Box: 1")
+});
+
+const btn2 = document.querySelector('#two');
+btn2.addEventListener('mouseover', () =>{
+    console.log("Box: 2")
+});
+
+const btn3 = document.querySelector('#three');
+btn3.addEventListener('mouseover', () =>{
+    console.log("Box: 3")
+});
+
+const btn4 = document.querySelector('#four');
+btn4.addEventListener('mouseover', () =>{
+    console.log("Box: 4")
+});
+
+const btn5 = document.querySelector('#five');
+btn5.addEventListener('mouseover', () =>{
+    console.log("Box: 5")
+});
+
+const btn6 = document.querySelector('#six');
+btn6.addEventListener('mouseover', () =>{
+    console.log("Box: 6")
+});
+
+const btn7 = document.querySelector('#seven');
+btn7.addEventListener('mouseover', () =>{
+    console.log("Box: 7")
+});
+
+const btn8 = document.querySelector('#eight');
+btn8.addEventListener('mouseover', () =>{
+    console.log("Box: 8")
+});
+
+const btn9 = document.querySelector('#nine');
+btn9.addEventListener('mouseover', () =>{
+    console.log("Box: 9")
+});
