27d26e6ca10bd454503dd14a9a102d5bd1a8e3f7
Froilan <froilan.i.fajardo@gmail.com>
2022-04-06 21:34:30 +0800

    modified etch-a-sketch

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-const btn1 = document.querySelector('#one');
-btn1.addEventListener('mouseover', () =>{
-    console.log("Box: 1")
-});
+const container = document.querySelector('.container');
 
-const btn2 = document.querySelector('#two');
-btn2.addEventListener('mouseover', () =>{
-    console.log("Box: 2")
-});
+var value = 50; //prompt("Enter value upto 100");
+var sizeRef = 100/value;
+var size = JSON.stringify(sizeRef)+"%";
 
-const btn3 = document.querySelector('#three');
-btn3.addEventListener('mouseover', () =>{
-    console.log("Box: 3")
-});
+for (var i=0; i<value*value; i++){
+    const blocks = document.createElement('div');
+    blocks.setAttribute('id', 'blocks');
+    blocks.setAttribute('class', "box "+ i);
+    blocks.style.width = size;
+    container.appendChild(blocks);
 
-const btn4 = document.querySelector('#four');
-btn4.addEventListener('mouseover', () =>{
-    console.log("Box: 4")
-});
+    var myArr = [];
 
-const btn5 = document.querySelector('#five');
-btn5.addEventListener('mouseover', () =>{
-    console.log("Box: 5")
-});
+    blocks.addEventListener('mousedown', () => {
+        myArr.pop();
+        myArr.unshift(1);
+        console.log(myArr[0]);
+    });
+    blocks.addEventListener('mouseup', () => {
+        myArr.pop();
+        myArr.unshift(0);
+        console.log(myArr[0]);
+    });
 
-const btn6 = document.querySelector('#six');
-btn6.addEventListener('mouseover', () =>{
-    console.log("Box: 6")
-});
+    blocks.addEventListener('mouseover', () => {
+        if (myArr[0] == 1) {
+            blocks.style.backgroundColor = 'black';
+        }
+    });
+    blocks.addEventListener('mousedown', () => {
+        if (myArr[0] == 1) {
+            blocks.style.backgroundColor = 'black';
+        }
+    });
+}
 
-const btn7 = document.querySelector('#seven');
-btn7.addEventListener('mouseover', () =>{
-    console.log("Box: 7")
-});
-
-const btn8 = document.querySelector('#eight');
-btn8.addEventListener('mouseover', () =>{
-    console.log("Box: 8")
-});
-
-const btn9 = document.querySelector('#nine');
-btn9.addEventListener('mouseover', () =>{
-    console.log("Box: 9")
-});
+/* const btn = document.querySelector('#box');
+btn.addEventListener('click', () => {
+    console.log('hey');
+}); */
